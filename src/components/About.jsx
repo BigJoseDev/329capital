212b890171796d5a5e31eb1aaa9988ca5b8dd104
BigJoseDev <aliemhejosemaria@gmail.com>
2025-02-01 19:04:01 +0100

    commit

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,7 +6,7 @@ const textContainerVariants = {
   hidden: {},
   visible: {
     transition: {
-      staggerChildren: 0.3,
+      staggerChildren: 0.2,
     },
   },
 };
@@ -16,7 +16,7 @@ const textItemVariants = {
   visible: {
     opacity: 1,
     y: 0,
-    transition: { duration: 0.2, ease: "easeOut" },
+    transition: { duration: 0.1, ease: "easeOut" },
   },
 };
 
