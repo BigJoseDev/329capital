354a09796a67d755c3d01f52294d0e6cee596d4d
BigJoseDev <aliemhejosemaria@gmail.com>
2025-02-01 19:02:01 +0100

    commit

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -16,7 +16,7 @@ const textItemVariants = {
   visible: {
     opacity: 1,
     y: 0,
-    transition: { duration: 0.6, ease: "easeOut" },
+    transition: { duration: 0.2, ease: "easeOut" },
   },
 };
 
